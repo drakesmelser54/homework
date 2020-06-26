6760276b21a37f272b469fb7473284107ceea07f
Drake Smelser <drakesmelser54@gmail.com>
2020-06-26 15:30:54 -0500

    add Week2Homework to folder

---
+++ Week2Homework.js
@@ -0,0 +1,31 @@
+//create 2 variables and set them to name of place and number of topping.//
+const pizzaPlace = "Olivie's on Shaw";
+let numberOfToppings = 9;
+
+//print the variables and their types//
+console.log(
+  pizzaPlace,
+  typeof pizzaPlace,
+  numberOfToppings,
+  typeof numberOfToppings
+);
+
+//create a template literal that uses variables to construct a short sentence//
+console.log(
+  `One of my favorite restaurants to go to in St. Louis is ${pizzaPlace}. It's this amazing combination of French and Italian pizza that utlizes ${numberOfToppings} toppings to make sure you're never tired of their menu!`
+);
+
+//create an if statement that prints "Quality Over Quantity" if you offer less than 10 toppings//
+//It will say "Whole Lot of Pizza" if you offer 10 or more toppings.//
+
+if (numberOfToppings < 10) {
+  console.log("Quality Over Quantity.");
+} else console.log("Whole Lot of Pizza!");
+
+//bonus challenge//
+//Use a Loop to print all of the even numbers from 1 to numberOfToppings (or 10)//
+for (let numberOfToppings = 1; numberOfToppings <= 10; numberOfToppings++) {
+  if (numberOfToppings % 2 === 0) {
+    console.log(numberOfToppings);
+  }
+}
